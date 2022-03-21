f4a2b7d37e5fceb2eb85d394c90f68efbaa9ffd4
niharparmar-deloitte <pyogeshbhai@deloitte.com>
2022-03-21 13:13:31 +0530

    Mini Assignment 6 solution

---
+++ mini_assignment_6/src/student_data.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+public class student_data {
+    public static void main(String[] args) throws Exception {
+//parsing a CSV file into Scanner class constructor
+        Scanner sc = new Scanner(new File("C:\\Users\\pyogeshbhai\\Documents\\StudentData.csv"));
+        sc.useDelimiter(",");   //sets the delimiter pattern
+        while (sc.hasNext())  //returns a boolean value
+        {
+            System.out.print(sc.next());  //find and returns the next complete token from this scanner
+        }
+        sc.close();
+    }
+}
